5963401b986f2fbc5bf6e94b2165201ef13e4622
huanghe <huanghe@C02S12ZCFVH5.local>
2017-07-11 10:22:52 +0800

    首页轮播图bug

--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {Carousel} from 'antd';
 import pic1 from '../../images/part1.jpg'
-import slideLog from '../../images/section1-logo.png'
+import slideLogo from '../../images/section1-logo.png'
 import './slide.css'
 
 export default class Slide extends React.Component{
 	render(){
 		return(
 			<div className='slide'>
-				<Carousel  className='slide'>
+				<Carousel  className='slide-wrapper'>
 					<div className='slide-img'><img  src={pic1} alt=""/></div>
 					<div className='slide-img'><img  src={pic1} alt=""/></div>
 				</Carousel>
-				<img className='slide-log' src={slideLog}></img>
+				<img className='slide-logo' src={slideLogo}></img>
 			</div>
 		)
 	}
